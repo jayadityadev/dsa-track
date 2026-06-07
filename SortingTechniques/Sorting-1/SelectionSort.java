f8a9ea6c3c4033941c269c77dfc268930b5042c9
Jayaditya Dev <jayadityadev261204@gmail.com>
2026-06-07 20:20:02 +0530

    Sorting Techniques -> 1 -> SelectionSort

--- SortingTechniques/Sorting-1/SelectionSort.java
+++ SortingTechniques/Sorting-1/SelectionSort.java
@@ -5,15 +5,29 @@ public class SelectionSort {
         System.out.println();
     }
 
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
     static void sort(int[] arr) {
-        
+        int i, j;
+        for (i = 0; i < arr.length-1; i++) {
+            int min = i;
+            for (j = i+1; j < arr.length; j++)
+                if (arr[j] < arr[min])
+                    min = j;
+            swap(arr, i, min);
+        }
     }
 
     public static void main(String[] args) {
         int[] arr = {13,46,24,52,20,9};
         System.out.print("Original Array: ");
         displayArr(arr);
-        System.out.println("Sorted Array: ");
+        System.out.print("Sorted Array: ");
+        sort(arr);
         displayArr(arr);
     }
 }
